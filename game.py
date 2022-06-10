734fbf21b2caf42c7e479b9e2644d8dc9246d5ec
Noam Shabat <92057540+Noamshabat1@users.noreply.github.com>
2022-06-10 17:11:46 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,3 @@
-#################################################################
-# FILE : apple.py
-# WRITER 1 : noam shabat , no.amshabat1 , 206515579
-# WRITER 2 : Eden Faingold, edenfaingold, 318227113
-# EXERCISE : intro2cs2 ex10 2021
-# DESCRIPTION: The main class of the game which incorporates all the elements in the game
-# STUDENTS I DISCUSSED THE EXERCISE WITH:
-# WEB PAGES I USED: stackoverflow.com
-#################################################################
-
 import game_parameters as gp
 from apple import Apple
 from bomb import Bomb
